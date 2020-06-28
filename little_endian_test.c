92437c0da0cf0b99865c1233d772a88d9e3a89df
binchoo <079111w@gmail.com>
2020-06-29 00:53:56 +0900

    remove binaries

--- little_endian_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-union DATA {int integer; char b[4];};
-int main()
-{
-    union DATA d;
-    d.integer = 0x0a0b0c0d;
-    printf("%d => %c %c %c %c", d.integer, d.b[0], d.b[1], d.b[2], d.b[3]);
-    return 0;
-}
